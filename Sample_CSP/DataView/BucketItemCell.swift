ca7962f0a148cf4382dcbe6b3fa04b5afb56edda
jairo13c <31705891+jairo13c@users.noreply.github.com>
2018-01-09 12:51:09 -0700

    created a navigation table to onect all windows

--- Sample_CSP/DataView/BucketItemCell.swift
+++ Sample_CSP/DataView/BucketItemCell.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-public class BucketItemCell: SKScene
+public class BucketItemCell: UITableViewCell
 {
-
     
+    @IBOutlet weak var bucketItemText: UILabel!
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    @IBOutlet weak var bucketItemSignature: UILabel!
+    
+}
+var bucketItem : BucketItem!
+{
+    disdSet
+        {
+            updateCellView()
+        }
+    private func randomEmoji()->String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1FC5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart +
+            Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji
+    }
 }
